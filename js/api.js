a1eec93a535424b1dcab79f82c32019990acc7f3
Victor Avila <victor.avila@venturplex.com>
2018-04-13 15:31:45 -0700

    css update

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#quote").on("click", function(){
     $.getJSON('https://talaikis.com/api/quotes/random/', function(json) {
-      $(".message").html('"' + json.quote + '"');
-      $(".author").html(` - ` + json.author);
-      $(".cat").html('categories: ' + json.cat)
+      $("#message").html('"' + json.quote + '"');
+      $("#author").html(` - ` + json.author);
+      $("#cat").html('categories: ' + json.cat)
     });
   });
 });
