141ba7509bd2590c0eb7050239e2bc2c729b5e92
Victor Avila <victor.avila@venturplex.com>
2018-04-13 15:27:58 -0700

    display author and cats

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("#quote").on("click", function(){
     $.getJSON('https://talaikis.com/api/quotes/random/', function(json) {
-      $(".message").html(JSON.stringify(json.quote));
+      $(".message").html('"' + json.quote + '"');
+      $(".author").html(` - ` + json.author);
+      $(".cat").html('categories: ' + json.cat)
     });
   });
 });
